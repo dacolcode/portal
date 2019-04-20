33e77aec523a5fe60ea76a0db5490b5a08f01808
DACol <datosabiertoscol@gmail.com>
2019-04-19 22:37:27 -0500

    se ajusta plantilla

--- dacolweb/urls.py
+++ dacolweb/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tablas', views.tablas, name='tablas'),
     path('dataset', views.dataset_general, name='dataset_general'),
 	path('dataset/<str:uuid>/', views.dataset_detail, name='dataset_detail'),
 ]
--- dacolweb/views.py
+++ dacolweb/views.py
@@ -4,14 +4,40 @@ from django.shortcuts import render, get_object_or_404
 import requests
 import pandas as pd
 
+#gráficas
+import matplotlib as pl
+import matplotlib.pyplot as plt
+
+def home(request):
+
+    context = {
+        'mensaje': "Aquí poner una intro bien guapa",
+    }
+
+    #return render(request, 'portal/dataset_intro.html', context)
+    return render(request, 'portal/index.html', context)
+
+def tablas(request):
+    return render(request, 'portal/tables.html')
+
 def dataset_detail(request, uuid):
     ## dataset
     data = pd.read_json('https://www.datos.gov.co/resource/'+uuid+'.json')
     metadata = requests.get('https://www.datos.gov.co/api/views/metadata/v1/'+uuid).json()
+    metadataPlus = requests.get('http://api.us.socrata.com/api/catalog/v1?ids='+uuid).json()
+
+    #derivados
+    derivados = requests.get('http://api.us.socrata.com/api/catalog/v1?derived_from'+uuid).json()
+
+    #otras mismo dueño
+    id_autor =  metadataPlus['results'][0]['owner']['id']
+    otros_autor = requests.get('http://api.us.socrata.com/api/catalog/v1?derived_from'+ id_autor).json()
+
 
     context = {
         'metadata': metadata,
         'data': data,
+        'metadataPlus': metadataPlus['results'][0]['owner']['id'],
     }
 
     return render(request, 'portal/dataset_detail.html', context)
@@ -23,24 +49,22 @@ def dataset_general(request):
     obtener_cantidad = requests.get('http://api.us.socrata.com/api/catalog/v1?domains=www.datos.gov.co&offset=0&limit=1').json()
     cantidad = obtener_cantidad['resultSetSize']
 
+    ## por categorias
+    facets = requests.get('http://api.us.socrata.com/api/catalog/v1/domains/www.datos.gov.co/facets').json()
+    categorias = facets[0]
+
 
     ## - traerlos todos para procesar y evaluar (métricas)
 
 
     ##Para mostrar se traen los 100 más recientes
     ####mostrar = requests.get('http://api.us.socrata.com/api/catalog/v1?domains=www.datos.gov.co&order=createdAt&offset=0&limit=10000').json() #intentando pintar 10000 maté mi servidor ¡ojo!
-    mostrar = requests.get('http://api.us.socrata.com/api/catalog/v1?domains=www.datos.gov.co&order=createdAt').json()
+    mostrar = requests.get('http://api.us.socrata.com/api/catalog/v1?domains=www.datos.gov.co&order=createdAt&only=datasets').json()
 
     context = {
         'mostrar': mostrar,
+        'categorias': categorias,
     }
 
     return render(request, 'portal/dataset_general.html', context)
 
-def home(request):
-
-    context = {
-        'otro': "Hola mundo",
-    }
-
-    return render(request, 'portal/dataset_intro.html', context)
